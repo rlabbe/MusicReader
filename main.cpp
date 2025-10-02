b3c1b125a61c4e7e49ab35f63c5fe23dc068c1d9
Roger Labbe <rlabbejr@gmail.com>
2025-10-02 09:44:47 -0700

    Enforce dark mode
    My icons were not designed for light mode, and the idea is no extra
    light on the desk from the app, so dark is best.

--- main.cpp
+++ main.cpp
@@ -61,6 +61,8 @@ int main(int argc, char *argv[])
     {
         QApplication app(argc, argv);
         app.setStyle("fusion");
+        QStyleHints *hints = QGuiApplication::styleHints();
+        hints->setColorScheme(Qt::ColorScheme::Dark);
 
         MusicReader w;
         w.show();
