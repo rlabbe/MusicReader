e96cd37d19a42bd9689ab7bf4fd167569ac90414
Roger Labbe <rlabbejr@gmail.com>
2025-11-01 07:27:26 -0700

    Do not allow ascii keys to search in bookmark panel
    it conflicts with shortcuts like 1/2, etc.

--- bookmark_treewidget.cpp
+++ bookmark_treewidget.cpp
@@ -38,6 +38,10 @@ void BookmarkTreeWidget::keyPressEvent(QKeyEvent* event)
         case Qt::Key_PageDown:
         case Qt::Key_Left:
         case Qt::Key_Right: QCoreApplication::sendEvent(main_window_, event); break;
-        default: QTreeWidget::keyPressEvent(event);
+        default:
+            if (event->text().isEmpty() || !event->text().at(0).isPrint())
+                QTreeWidget::keyPressEvent(event);
+            else
+                event->ignore();
     }
 }
